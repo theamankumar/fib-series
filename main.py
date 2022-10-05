cc4d759df921391d11b8d6ad2af8db78c9cbfda9
Aman Kumar <70056787+amankumr@users.noreply.github.com>
2022-10-05 14:46:48 +0530

    Update main.py
    Enhanced Code

--- main.py
+++ main.py
@@ -1,25 +1,11 @@
-# Function for nth Fibonacci number
-def Fibonacci(n):
-
-	# Check if input is 0 then it will
-	# print incorrect input
-	if n < 0:
-		print("Incorrect input")
-
-	# Check if n is 0
-	# then it will return 0
-	elif n == 0:
-		return 0
-
-	# Check if n is 1,2
-	# it will return 1
-	elif n == 1 or n == 2:
-		return 1
-
-	else:
-		return Fibonacci(n-1) + Fibonacci(n-2)
+def fib(n):
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
 
 # Driver Program
-a = input("Enter The 'n':")
-print(Fibonacci(a))
+x = input("Enter The 'n':")
+print(fib(x))
 
